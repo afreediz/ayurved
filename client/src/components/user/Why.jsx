f52cc2ee5b96b25ef64f564cd1c55d5f2b172287
afreediz <afreedisulfiker@gmail.com>
2024-07-18 15:05:20 +0530

    updated why

--- client/src/components/user/Why.jsx
+++ client/src/components/user/Why.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Why = () => {
   return (
-    <div className='w-full text-white bg-green-900 flex flex-col justify-center items-center p-10 relative'>
+    <div
+  style={{
+    backgroundImage: "url('/images/why/bg.jpeg')",
+    backgroundSize: "cover", // Ensure the entire image fits within the div
+    backgroundPosition: "center", // Center the image,
+    backgroundRepeat: "no-repeat",
+    width: "100%", // Ensure the div takes full width
+    height: "auto" // Let height adjust based on content or use a fixed height,
+  }}
+  className="w-full text-white flex flex-col justify-center items-center p-10 my-10 relative z-[-1]"
+>
+<div className="absolute inset-0 bg-black opacity-50 z-[-1]"></div>
       <h1 className=' pb-6'>Why NAVJEEVANA</h1>
       <div className="items flex justify-center gap-16 lg:gap-60 flex-wrap">
         <div className="item text-center">
