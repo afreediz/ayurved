02da880a227f087b84a00c16c75dab8b9b70ed30
afreediz <afreedisulfiker@gmail.com>
2024-07-22 22:41:56 +0530

    updated footer

--- client/src/components/user/PhoneOptions.jsx
+++ client/src/components/user/PhoneOptions.jsx
@@ -15,17 +15,17 @@ const PhoneOptions = () => {
   }
 
   return (
-    <div className='fixed bottom-0 left-0 right-0 flex md:hidden justify-around gap-4 z-10 bg-white py-1 border-slate-500 border-t'>
+    <div className='fixed bottom-0 left-0 right-0 flex md:hidden justify-around gap-4 z-10 bg-white border-gray-800 border-t text-slate-700 py-1'>
         <Link to={"/"} className="flex flex-col items-center">
-          <FaHome className='text-slate-800 text-3xl'/>
+          <FaHome className=' text-2xl'/>
           <span>Home</span>
         </Link>
         <Link to={"/allproducts"} className="flex flex-col items-center">
-          <FaShoppingCart className='text-slate-800 text-3xl'/>
+          <FaShoppingCart className=' text-2xl'/>
           <span>Shop</span>
         </Link>
         <Link to={user? "/profile" : "/login"} className="flex flex-col items-center" onMouseEnter={() => setProfileOpen(true)} onMouseLeave={() => setProfileOpen(false)}>
-            <FaUserCircle className='text-slate-800 text-3xl' />
+            <FaUserCircle className=' text-2xl' />
             <span>Account</span>
         </Link>
     </div>
