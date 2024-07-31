daceb1f9dc1ac75d14f4309eedb270a74ab385e6
afreediz <afreedisulfiker@gmail.com>
2024-08-01 00:36:32 +0530

    ui updates

--- api/middlewares/errorHandler.js
+++ api/middlewares/errorHandler.js
@@ -1,28 +1,35 @@
 module.exports = (err, req, res, next)=> {
     console.log(err);
-    if(err.code == 11000){
-        err.message = "Duplicate Field Value Enter"
-    }
-    switch(err.name){
-        case "SyntaxError":
-            err.message = "Unexpected syntax"
-        case "CastError":
-            err.message = "Please provide a valid id"
-        case "TokenExpiredError":
-            err.message = "Session expired. Please signin again"
-        case "JsonWebTokenError":
-            err.message = "Signin required"
-        default:
-    }
     if (err.message && err.message.startsWith("CUSTOM ERROR: ")) {
         return res.status(400).json({
             success: false,
             message: err.message.replace("CUSTOM ERROR: ", "")
         })
     }else{
+        switch(err.name){
+            case "SyntaxError":
+                err.message = "Unexpected syntax"
+                break
+            case "CastError":
+                err.message = "Please provide a valid id"
+                break
+            case "TokenExpiredError":
+                err.message = "Session expired. Please signin again"
+                break
+            case "JsonWebTokenError":
+                err.message = "Signin required"
+                break
+            default:
+                err.message = "Internal server error"    
+                
+                if(err.code == 11000){
+                    err.message = "Duplicate Field Value Enter"
+                }
+                break
+        }
         return res.status(500).json({
             success: false,
-            message: "Internal server error"
+            message: err.message
         })
     }
 }
