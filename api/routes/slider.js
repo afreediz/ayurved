eaf6ad10c4b472c953cf890830892401bb7eff98
afreediz <afreedisulfiker@gmail.com>
2025-04-20 21:32:51 +0530

    Updates

--- api/routes/slider.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { getSliders, createSlider, deleteSlider } = require('../controllers/slider')
-const { isAuthenticated, isAdmin } = require('../middlewares/isAuth')
-
-router.get('/', getSliders)
-router.post('/', isAuthenticated, isAdmin, createSlider)
-router.delete('/:id', isAuthenticated, isAdmin, deleteSlider)
-
-module.exports = router
